202665b666eddaf8282acdb2bbef54a9291028bc
llamitasalex <buzondealexll@gmail.com>
2019-10-22 21:59:08 +0200

    API

---
+++ server.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
